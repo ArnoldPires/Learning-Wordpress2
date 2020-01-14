e25417606c1b34666986c1cfcec63bae37590b57
ArnoldPires <arnold.pires92@gmail.com>
2020-01-14 15:58:25 -0500

    started with some javascript

---
+++ javascript/age-calculator/main.js
@@ -0,0 +1,4 @@
+let age = prompt("How old are you?");
+alert("Okay, you are " + age);
+let days = age * 365.25;
+alert("You have been alive for " + days + " days" + " Congratz!");
---
+++ javascript/testing/main.js
@@ -0,0 +1,34 @@
+// 100 % 3 = 1
+
+//("blah" + "blah")[6]
+
+//This example counts how many letters are in "hello" and "hi" and divdes them
+//"hello".length % "hi\\".length
+
+
+//Variables are simply containers that store values
+//They follow this pattern:
+var yourVaribleName = yourValue;
+var yourValue = "yourVaribleName";
+
+//They can store all of the values we've seen
+var name = "Bob";
+var secretNumber = 73;
+var isCute = true;
+
+//Recall the stored value by calling the variable name
+var name = "Bob";
+"Hello there" + name  //"hello there Bob"
+
+var num = 37;
+num + 3 + 10 //50
+
+//we can also update existing Variables
+var name = "Arnold";
+name = "Peter";
+
+alert("hi");
+console.log("hi");
+
+var userName = prompt("what is your name?");
+alert("Nice to meet you, " + userName);
