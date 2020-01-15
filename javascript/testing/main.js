d13d3117b9adc1828b71ec1ebb394154d6cfaa9d
ArnoldPires <arnold.pires92@gmail.com>
2020-01-14 19:38:43 -0500

    next update

--- javascript/testing/main.js
+++ javascript/testing/main.js
@@ -32,3 +32,33 @@ console.log("hi");
 
 var userName = prompt("what is your name?");
 alert("Nice to meet you, " + userName);
+
+var x = 10;
+var y = "a"
+y === "b" || x >= 10;
+// y === "b" is false but x >= 10 is true. So this whole thing remains as true
+
+var x = 3;
+var y = 8;
+!(x == "3" || x === y) && !(y != 8 && x <= y)
+//         leftside     and    Rightside
+//  true                      False     true
+// (If the left side is true, it doesn't matter whats on the right. The leftside is true)
+//However, there is a ! in the front. Which means the whole thing is false
+
+
+//False values: (Thise values will always be false. Anything else, is usually true)
+// False
+//0
+//""
+//null
+//undefined
+//NaN
+
+var str = ""
+var msg = "haha!"
+var isFunny = "false"
+!((str || msg) && isFunny);
+//leftside        rightside
+//false   True    false
+// However, because ! is there, the whole thing is false
