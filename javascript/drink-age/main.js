c6e931cd41712d05732aa608453fd1cffd75521e
ArnoldPires <arnold.pires92@gmail.com>
2020-01-18 12:01:42 -0500

    pushing changes

--- javascript/drink-age/main.js
+++ javascript/drink-age/main.js
@@ -1,7 +1,14 @@
-let age = prompt("How are are you?");
-  if(age < 21) {
-  alert("okay come in, you are old enough");
-    else (age <= 21); {
-      alert("You are too young");
-    }
+//If age is negative print error message
+//if age is 21 printer happy birthday
+//if age is odd print your age is odd
+
+let age = prompt("How old are you?");
+if (age < 21) {
+  alert("You are not old enough.");
+} else if (age == 21) {
+  alert("Happy Birthday! Come on in.");
+} else if (age >= 100 ) {
+  alert("What the hell! How are you still alive!?")
+}else {
+  alert("Come on in.");
 };
